46bfef002c6568d36ca87afc664461ca3e3bcddd
Bráulio Silva <braulio.jose.silva@gmail.com>
2021-11-15 09:05:20 +0000

    List

--- src/components/TickerLinks.js
+++ src/components/TickerLinks.js
@@ -2,7 +2,6 @@ import { List, ListItem, ListItemText, ListItemIcon } from "@mui/material";
 import { StarBorder } from "@mui/icons-material";
 
 export default function TickerLinks({ bookmarks, ticker }) {
-  console.log(ticker);
   return (
     <List>
       {bookmarks.map((bookmark) => {
